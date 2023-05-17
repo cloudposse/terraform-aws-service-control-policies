d08d97cf597d069e75694163c5796f344c0cee46
Max Lobur <max_lobur@outlook.com>
2023-05-17 16:36:06 +0300

    Delete examples/complete directory

--- examples/complete/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "organizations_policy_id" {
-  value       = module.service_control_policies.organizations_policy_id
-  description = "The unique identifier of the policy"
-}
-
-output "organizations_policy_arn" {
-  value       = module.service_control_policies.organizations_policy_arn
-  description = "Amazon Resource Name (ARN) of the policy"
-}
