b382350ac00173e7f05cd132f3b08e8975bcbde6
Max Lobur <max_lobur@outlook.com>
2023-06-26 23:18:17 +0300

    Support AWS Provider V5

--- README.md
+++ README.md
@@ -178,7 +178,7 @@ Available targets:
 
 | Name | Version |
 |------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13.0 |
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3 |
 | <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 3.0 |
 | <a name="requirement_local"></a> [local](#requirement\_local) | >= 1.3 |
 
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "organizations_policy_id" {
-  value       = join("", aws_organizations_policy.this.*.id)
+  value       = join("", aws_organizations_policy.this[*].id)
   description = "The unique identifier of the policy"
 }
 
 output "organizations_policy_arn" {
-  value       = join("", aws_organizations_policy.this.*.arn)
+  value       = join("", aws_organizations_policy.this[*].arn)
   description = "Amazon Resource Name (ARN) of the policy"
 }
