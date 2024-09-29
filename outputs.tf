a7fe3358bac02e1ee20d5a8af466b40b6fea99c4
Igor Rodionov <goruha@gmail.com>
2024-09-29 19:04:35 +0200

    Create policy per file

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "organizations_policy_id" {
-  value       = one([ for i in aws_organizations_policy.this : i.id])
+  value       = one([for i in aws_organizations_policy.this : i.id])
   description = "The unique identifier of the policy"
 }
 
 output "organizations_policy_arn" {
-  value       = one([ for i in aws_organizations_policy.this : i.arn])
+  value       = one([for i in aws_organizations_policy.this : i.arn])
   description = "Amazon Resource Name (ARN) of the policy"
 }
 
 output "organizations_policy_ids" {
-  value       = [ for i in aws_organizations_policy.this : i.id]
+  value       = [for i in aws_organizations_policy.this : i.id]
   description = "The unique identifier of the policies"
 }
 
 output "organizations_policy_arns" {
-  value       = [ for i in aws_organizations_policy.this : i.arn]
+  value       = [for i in aws_organizations_policy.this : i.arn]
   description = "Amazon Resource Name (ARN) of the policies"
 }
