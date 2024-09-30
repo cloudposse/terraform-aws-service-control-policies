831af5d6465f19c8b8bbc8e359a5a7c8b31b0b1a
Igor Rodionov <goruha@gmail.com>
2024-09-30 22:51:03 +0300

    Run tests per policy (#62)

--- test/src/examples_complete_test.go
+++ test/src/examples_complete_test.go
@@ -6,28 +6,24 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"os"
+	"path"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
 )
 
 func cleanup(t *testing.T, terraformOptions *terraform.Options, tempTestFolder string) {
 	terraform.Destroy(t, terraformOptions)
-	os.RemoveAll(tempTestFolder)
 }
 
-// Test the Terraform module in examples/complete using Terratest.
-func TestExamplesComplete(t *testing.T) {
-	t.Parallel()
+func applyPolicies(t *testing.T, tempTestFolder string, policyFile string) {
 	randID := strings.ToLower(random.UniqueId())
 	attributes := []string{randID}
 
-	rootFolder := "../../"
-	terraformFolderRelativeToRoot := "examples/complete"
 	varFiles := []string{"fixtures.us-east-2.tfvars"}
-
-	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
-
 	terraformOptions := &terraform.Options{
 		// The path to where our Terraform code is located
 		TerraformDir: tempTestFolder,
@@ -36,6 +32,7 @@ func TestExamplesComplete(t *testing.T) {
 		VarFiles: varFiles,
 		Vars: map[string]interface{}{
 			"attributes": attributes,
+			"list_config_paths": []string{policyFile},
 		},
 	}
 	// Keep the output quiet
@@ -58,4 +55,29 @@ func TestExamplesComplete(t *testing.T) {
 	organizationsPolicyArn := terraform.Output(t, terraformOptions, "organizations_policy_arn")
 	// Verify we're getting back the outputs we expect
 	assert.Contains(t, organizationsPolicyArn, "/service_control_policy/"+organizationsPolicyId)
+}
+
+
+// Test the Terraform module in examples/complete using Terratest.
+func TestExamplesComplete(t *testing.T) {
+	rootFolder := "../../"
+	terraformFolderRelativeToRoot := "examples/complete"
+
+	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
+	defer os.RemoveAll(tempTestFolder)
+
+	catalogRegEx, e := regexp.Compile("^.+\\.(yaml)$")
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	e = filepath.Walk(path.Join(path.Dir(rootFolder), "./catalog"), func(path string, info os.FileInfo, err error) error {
+		if err == nil && catalogRegEx.MatchString(info.Name()) {
+			applyPolicies(t, tempTestFolder, path)
+		}
+		return nil
+	})
+	if e != nil {
+		log.Fatal(e)
+	}
 }
